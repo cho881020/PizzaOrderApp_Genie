85ceada52fa4c1115885f5da07f5418fd198cf73
Geine <cho881020@gmail.com>
2021-05-24 22:59:09 +0900

    뷰페이저 + 탭레이아웃 연결까지

--- app/src/main/java/com/neppplus/pizzaorderapp_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_genie/MainActivity.kt
@@ -2,9 +2,12 @@ package com.neppplus.pizzaorderapp_genie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.pizzaorderapp_genie.adapters.MainViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +28,13 @@ class MainActivity : BaseActivity() {
 
 //        뷰페이저 작업
 
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mvpa
+
 
 //        탭레이아웃과 연결
 
-//        mainTabLayout.setupWithViewPager()
+        mainTabLayout.setupWithViewPager(mainViewPager)
 
 
     }
