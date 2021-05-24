578e7949ed68f8eb885f2d215ecd47eff13ad97d
Geine <cho881020@gmail.com>
2021-05-24 15:38:13 +0900

    메인화면이 -> BaseActivity 기반으로 동작하도록 설정.

--- app/src/main/java/com/neppplus/pizzaorderapp_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_genie/MainActivity.kt
@@ -3,9 +3,26 @@ package com.neppplus.pizzaorderapp_genie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    override fun setupEvents() {
+
+//        버튼 클릭등의 이벤트 관련 코드 모음.
+
+    }
+
+    override fun setValues() {
+
+//        화면에 데이터를 표시하기 위한 코드 모음.
+
+    }
+
+
 }
