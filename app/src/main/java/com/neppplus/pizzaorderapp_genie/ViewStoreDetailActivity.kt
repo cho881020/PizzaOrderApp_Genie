3f9170ef58e3a955a9f3244c865a8e3ff25d6feb
Geine <cho881020@gmail.com>
2021-05-25 17:58:44 +0900

    상세 화면 실제 데이터 반영

--- app/src/main/java/com/neppplus/pizzaorderapp_genie/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_genie/ViewStoreDetailActivity.kt
@@ -2,14 +2,20 @@ package com.neppplus.pizzaorderapp_genie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.neppplus.pizzaorderapp_genie.datas.Store
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : BaseActivity() {
+
+    lateinit var mStoreData : Store
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
         setupEvents()
         setValues()
+
     }
 
     override fun setupEvents() {
@@ -17,6 +23,13 @@ class ViewStoreDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mStoreData = intent.getSerializableExtra("storeData") as Store
+
+        Glide.with(mContext).load(mStoreData.logoURL).into(storeLogoImg)
+
+        storePhoneNumTxt.text = mStoreData.phoneNum
+        storeNameTxt.text = mStoreData.name
 
     }
 
