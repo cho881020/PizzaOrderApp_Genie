89685452674a653e6aebc4a153739513b517e077
Geine <cho881020@gmail.com>
2021-05-25 18:15:30 +0900

    TedPermission 활용 -> 전화 권한 획득 후 전화걸기

--- app/src/main/java/com/neppplus/pizzaorderapp_genie/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_genie/ViewStoreDetailActivity.kt
@@ -1,10 +1,14 @@
 package com.neppplus.pizzaorderapp_genie
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.neppplus.pizzaorderapp_genie.datas.Store
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
@@ -24,9 +28,26 @@ class ViewStoreDetailActivity : BaseActivity() {
 
         callPhoneBtn.setOnClickListener {
 
-            val myUri = Uri.parse("tel:${mStoreData.phoneNum}")
-            val myIntent = Intent(Intent.ACTION_CALL, myUri)
-            startActivity(myIntent)
+            val permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    val myUri = Uri.parse("tel:${mStoreData.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "전화 연결 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.with(mContext)
+                    .setPermissionListener(permissionListener)
+                    .setDeniedMessage("[설정] 에서 전화 권한을 켜주세요.")
+                    .setPermissions(Manifest.permission.CALL_PHONE)
+                    .check()
+
+
 
         }
 
