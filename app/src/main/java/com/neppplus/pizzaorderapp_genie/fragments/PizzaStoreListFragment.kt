967972c990725b83716b58950a0f052deff77edc
Geine <cho881020@gmail.com>
2021-05-25 17:13:46 +0900

    실제 데이터 추가 + 어댑터 / 리스트뷰 연결

--- app/src/main/java/com/neppplus/pizzaorderapp_genie/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_genie/fragments/PizzaStoreListFragment.kt
@@ -6,8 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.pizzaorderapp_genie.R
+import com.neppplus.pizzaorderapp_genie.adapters.PizzaStoreAdapter
+import com.neppplus.pizzaorderapp_genie.datas.Store
+import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
 
 class PizzaStoreListFragment : Fragment() {
+
+    val mPizzaStoreDataList = ArrayList<Store>()
+
+    lateinit var mPizzaStoreAdapter : PizzaStoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +29,18 @@ class PizzaStoreListFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         
 //        동작 관련 코드
-        
+
+        mPizzaStoreDataList.add(  Store("A피자가게", "1111-5555", "https://cdn3.iconfinder.com/data/icons/street-food-and-food-trucker-1/64/pizza-fast-food-bake-bread-128.png")  )
+        mPizzaStoreDataList.add(  Store("B피자가게", "2222-3333", "https://cdn1.iconfinder.com/data/icons/cartoon-snack/128/pizza-128.png")  )
+        mPizzaStoreDataList.add(  Store("C피자가게", "3333-7777", "https://cdn3.iconfinder.com/data/icons/food-set-3/91/Food_C219-128.png")  )
+        mPizzaStoreDataList.add(  Store("D피자가게", "4444-2222", "https://cdn0.iconfinder.com/data/icons/fastfood-31/64/pizza-italian-food-fast-fastfood-cheese-piece-128.png")  )
+
+
+        mPizzaStoreAdapter = PizzaStoreAdapter(requireContext(), R.layout.pizza_store_list_item, mPizzaStoreDataList)
+
+        pizzaStoreListView.adapter = mPizzaStoreAdapter
+
+
     }
 
 }
